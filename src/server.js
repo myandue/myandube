e5425b408268ebf811f6683a0dd007e1bfed8739
myandue <97776790+myandue@users.noreply.github.com>
2022-05-27 10:39:15 +0900

    creating a middleware 'logger'

--- src/server.js
+++ src/server.js
@@ -5,9 +5,15 @@ const app = express();
 const PORT = 4000;
 
 const handleHome = (req, res) => {
-    return res.send("hi");
+    return res.send("Welcome");
 }
 
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+app.use(logger);
 app.get("/", handleHome);
 
 const handleStart = () => {
